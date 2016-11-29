c2bc899e05013dc974b79b4a6525f765fd4978d6
Aude Lejuez <dusterherz@gmail.com>
2016-11-29 16:28:57 +0100

    Add notes

--- beep_boop_berry/__init__.py
+++ beep_boop_berry/__init__.py
@@ -22,6 +22,15 @@ class BeepBoop():
     Attributes :
         pin (int): the pin where the buzzer is connected
     """
+    class Note(IntEnum):
+        A = 440
+        B = 494
+        C = 262
+        D = 294
+        E = 330
+        F = 349
+        G = 392
+
     def __init__(self, pin):
         self.pin = pin
         GPIO.setmode(GPIO.BCM)
@@ -29,11 +38,29 @@ class BeepBoop():
 
     def beep(self, time):
         """
-            Make your beeper to sing a small *Beep*.
+            Make your beeper sing a small *Beep*.
             time is the duration of the beep in seconds
         """
-        pitch = 440
-        delay = 1 / pitch / 2
+        self.note(BeepBoop.Note.A, time)
+
+    def beeps(self, timeOn, timeOff, repeat):
+        """
+            Make your beeper sing multiples *Beep*.
+            time is the duration of the beep in seconds
+            repeat correspond to the numer of time the beeper must beep.
+
+        """
+
+        notes(BeepBoop.Note.A, timeOn, timeOff, repeat)
+
+    def note(self, note, time):
+        """
+            Make your beeper sing a single note.*
+            note is a note from the enum Note
+            time is the duration he the beep in seconds
+        """
+
+        delay = 1 / note / 2
         cycles = int(time * pitch)
 
         for i in range(cycles):
@@ -42,14 +69,8 @@ class BeepBoop():
             GPIO.output(self.pin, False)
             sleep(delay)
 
-    def beeps(self, timeOn, timeOff, repeat):
-        """
-            Make your beeper to sing multiples *Beep*.
-            time is the duration of the beep in seconds
-            repeat correspond to the numer of time the beeper must beep.
-
-        """
+    def notes(self, note, timeOn, timeOff, repeat):
 
         for i in range(repeat):
-            self.beep(timeOn)
+            note(note, timeOn)
             sleep(timeOff)
