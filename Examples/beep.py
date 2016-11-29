d20b94c878304532e7fe4544bca7f91636d239d6
Aude Lejuez <dusterherz@gmail.com>
2016-11-29 16:36:08 +0100

    add notes in example

--- Examples/beep.py
+++ Examples/beep.py
@@ -4,7 +4,7 @@ sys.path.insert(0, '..')
 from beep_boop_berry import BeepBoop
 
 beep = BeepBoop(2)
-beep.beep(1)
-beep.beeps(1, 1, 1)
+beep.beep(0.5)
+beep.beeps(0.1, 0.5, 2)
 beep.note(BeepBoop.Note.A, 1)
-beep.note(BeepBoop.Note.B, 2)
+beep.notes(BeepBoop.Note.c, 0.5, 0.5, 4)
