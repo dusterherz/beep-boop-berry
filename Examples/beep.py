eb3bde192f7186551dada7ff4f923c28c5681d8d
Aude Lejuez <dusterherz@gmail.com>
2016-11-29 16:56:15 +0100

    Replace dictionnary

--- Examples/beep.py
+++ Examples/beep.py
@@ -8,6 +8,6 @@ beep.beep(0.5)
 beep.beeps(0.1, 0.5, 2)
 beep.note(BeepBoop.Note.A, 1)
 beep.notes(BeepBoop.Note.C, 0.5, 0.5, 4)
-beep.song({{"note"=BeepBoop.Note.G, "time"=1}, {"note"=BeepBoop.Note.G, "time"=1}, {"note"=BeepBoop.Note.G, "time"=1},
- {"note"=BeepBoop.Note.E, "time"=0.5}, {"note"=BeepBoop.Note.B, "time"=0.5}, {"note"=BeepBoop.Note.G, "time"=1},
- {"note"=BeepBoop.Note.E, "time"=0.5}, {"note"=BeepBoop.Note.B, "time"=0.5}, {"note"=BeepBoop.Note.G, "time"=1.5})
+beep.song({{"note": BeepBoop.Note.G, "time": 1}, {"note": BeepBoop.Note.G, "time": 1}, {"note": BeepBoop.Note.G, "time": 1},
+ {"note": BeepBoop.Note.E, "time": 0.5}, {"note": BeepBoop.Note.B, "time": 0.5}, {"note": BeepBoop.Note.G, "time": 1},
+ {"note": BeepBoop.Note.E, "time": 0.5}, {"note": BeepBoop.Note.B, "time": 0.5}, {"note": BeepBoop.Note.G, "time": 1.5})
